9d14f664f4cc83ff78e59717fda2ae1bae1243cb
Tsaregorodtsev <65504823+tsar123@users.noreply.github.com>
2024-07-11 17:04:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,8 @@ best_params = {
      'bootstrap': True, 'max_depth': 40, 'max_features': 'sqrt', 'min_samples_leaf': 1, 'min_samples_split': 2, 'n_estimators': 300
 }
 
-best_rf_model = RandomForestClassifier(n_estimators=300, max_depth=30, random_state=42, max_features='auto', min_samples_leaf=1, min_samples_split=2)
-# best_rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+# best_rf_model = RandomForestClassifier(n_estimators=300, max_depth=30, random_state=42, max_features='auto', min_samples_leaf=1, min_samples_split=2)
+best_rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
 best_rf_model.fit(X_train, y_train)
 
 scaler = StandardScaler()
